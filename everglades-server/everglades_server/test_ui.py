589dd7a5463a9c806a287d422bafa89ae82624dd
Zack-N <zneidig@aol.com>
2021-01-26 18:33:07 -0500

    Completely redone squad creation UI Visually a lot more appealling and error proof. Not quite perfect, but a huge improvement.

--- everglades-server/everglades_server/test_ui.py
+++ everglades-server/everglades_server/test_ui.py
@@ -4,153 +4,295 @@ from CreateJsonData import *
 
 thismodule = sys.modules[__name__]
 
-squad1= []
-squad2= []
-squad3= []
-squad4= []
-squad5= []
-squad6= []
-squad7= []
-squad8= []
-squad9= []
-squad10= []
-squad11= []
-squad12= []
+numSquads = 12
+squadUnits = []
+squadNums = []
+tempSquadUnits = []
+tempSquadNums = []
+selectIndex = -1
 
-def helloWorld():
-    lbl_result["text"] = f"Hello World"
+for x in range(0, numSquads):
+    squadUnits.append([])
+    squadNums.append([])
 
-def clearSquad():
-    sel_squad_list['text'] = ""
+def refreshDropDown():
+    ## Remove current dropdown options
+    sel_squad_dd['menu'].delete(0, 'end')
+    ## Add new options
+    for x in range (1, numSquads+1):
+        sel_squad_dd['menu'].add_command(label=str(x), command=tk._setit(ddtext, str(x)))
+    ## Remove unit selection
+    global selectIndex
+    selectIndex = -1
 
-def loadSquad():
-    line0 = "if len(squad" + sel_squad_dd["text"] + ") == 0:\n   sel_squad_list['text'] = ''\n"
-    line1 = "for i, x in enumerate(squad" + sel_squad_dd["text"] + "):\n"
-    line2 = "   if i == 0:\n"
-    line3 = "      sel_squad_list['text'] = x\n"
-    line4 = "   else:\n"
-    line5 = "      sel_squad_list['text'] += ', ' + x"
-    exec(line0 + line1 + line2 + line3 + line4 + line5)
+def updateUnitList():
+    ## Get index of current squad
+    squadIndex = int(ddtext.get())-1
+    ## Clear listbox of units
+    curr_squad.delete(0, tk.END)
+    ## Repopulate with up-to-date units and counts
+    if len(squadUnits[squadIndex]) > 0:
+        for x in range(0,len(squadUnits[squadIndex])):
+            curr_squad.insert(tk.END, str(squadUnits[squadIndex][x]) + " ("+str(squadNums[squadIndex][x])+")")
 
-def saveSquad():
-    currentSquad = sel_squad_list['text'].split(", ")
-    arrayName = "squad" + sel_squad_dd["text"]
-    setattr(thismodule, arrayName, currentSquad[:])
+def addSquad():
+    global numSquads
+    global squadUnits
+    global squadNums
 
-def addUnit():
-    unitName = ent_unitname.get()
-    if unitName == "":
+    ## Append empty squad
+    numSquads+=1
+    squadUnits.append([])
+    squadNums.append([])
+    ## Jump to new squad
+    ddtext.set(str(numSquads))
+    ## Update dropdown & listbox
+    refreshDropDown()
+    updateUnitList()
+
+def deleteSquad():
+    global numSquads
+    global squadUnits
+    global squadNums
+
+    if numSquads == 1:
         return
     
-    textofnum = ent_unitnum.get()
-    if int(textofnum)>= 0 or int(textofnum) < 0:
-        number = int(textofnum)
-        for i in range(number):
-            if len(sel_squad_list['text']) == 0:
-                sel_squad_list['text'] = unitName
-            else:
-                sel_squad_list['text'] += ", " + unitName
+    ## Get current index
+    squadIndex = int(ddtext.get())-1
+    ## Pop index & update counter
+    squadUnits.pop(squadIndex)
+    squadNums.pop(squadIndex)
+    numSquads-=1
+    ## Jump down an index if necessary
+    if int(ddtext.get()) > numSquads:
+       ddtext.set(numSquads)
+    ## Update dropdown and listbox
+    refreshDropDown()
+    updateUnitList()
+
+def copySquad():
+    global squadUnits
+    global squadNums
+    global tempSquadUnits
+    global tempSquadNums
+    
+    ## Get current squad index
+    squadIndex = int(ddtext.get())-1
+    ## Store current state
+    tempSquadUnits = squadUnits[squadIndex]
+    tempSquadNums = squadNums[squadIndex]
+
+def pasteSquad():
+    global squadUnits
+    global squadNums
+    global tempSquadUnits
+    global tempSquadNums
+    
+    ## Get current squad index
+    squadIndex = int(ddtext.get())-1
+    ## Replace current squad with stored squad
+    squadUnits[squadIndex] = tempSquadUnits
+    squadNums[squadIndex] = tempSquadNums
+    ## Update list of units in current squad
+    updateUnitList()
+    ## Remove unit selection
+    global selectIndex
+    selectIndex = -1
+
+def deleteUnit():
+    global squadUnits
+    global squadNums
+    global selectIndex
+    
+    if selectIndex > -1:
+        squadIndex = int(ddtext.get())-1
+        squadUnits[squadIndex].pop(selectIndex)
+        squadNums[squadIndex].pop(selectIndex)
+    ## Update list of units in current squad
+    updateUnitList()
+    ## Remove unit selection
+    selectIndex = -1
+
+def addUnit():
+    global squadUnits
+    global squadNums
+    # Get index of squad to add to
+    squadIndex = int(ddtext.get())-1
+    # Add 1 of unit
+    squadUnits[squadIndex].append(nameVar.get())
+    squadNums[squadIndex].append(1)
+    # Update ListBox
+    updateUnitList()
+    ## Remove unit selection
+    global selectIndex
+    selectIndex = -1
+
+def updateSelNum(*args):
+    global selectIndex
+    if curr_squad.curselection() != ():
+        squadIndex = int(ddtext.get())-1
+        selectIndex = curr_squad.curselection()[0]
+        numVar.set(squadNums[squadIndex][selectIndex])
+    else:
+        if selectIndex < 0:
+            numVar.set("")
+
+def checkNumInput(S):
+    if S in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '']:
+        return True
+    print(S)
+    window.bell() # .bell() plays that ding sound telling you there was invalid input
+    return False
+
+def updateNum():
+    global selectIndex
+    squadIndex = int(ddtext.get())-1
+    squadNums[squadIndex][selectIndex] = numVar.get()
+    updateUnitList()
 
 def generateJSON():
-    loadout = [squad1, squad2, squad3, squad4, squad5, squad6, squad7, squad8, squad9, squad10, squad11, squad12]
-    playerIdentifier = int(ent_playnum.get())
+    global squadUnits
+    global squadNums
+    global numSquads
+    loadout = []
+    
+    for squadNumber in range(0,numSquads):
+        loadout.append([])
+        unitslength = len(squadUnits[squadNumber])
+        for unitInList in range(0,unitslength):
+            for unitNumber in range(0, int(squadNums[squadNumber][unitInList])):
+                loadout[squadNumber].append(squadUnits[squadNumber][unitInList])
+            
+    playerIdentifier = int(ddtext2.get())
+    #print(loadout)
+    #print(playerIdentifier)
     GenerateJsonFileLoadout(loadout, playerIdentifier)
 
 # Set-up the window and frames
 window = tk.Tk()
-window.title("EVERGLADES Function Tester")
+window.title("EVERGLADES Squad Creator UI")
 window.resizable(width=True, height=True)
-top_frame = tk.Frame(master=window)
-top_frame.grid(row=0, column=0, padx=10, pady=10)
-mid_frame = tk.Frame(master=window)
-mid_frame.grid(row=1, column=0, padx=10, pady=10)
-bot_frame = tk.Frame(master=window)
-bot_frame.grid(row=2, column=0, padx=10, pady=10)
-foot_frame = tk.Frame(master=window)
-foot_frame.grid(row=3, column=0, padx=10, pady=10)
 
-# Create the test Button and result display Label
-btn_test = tk.Button(
-    master=top_frame,
-    text="Test Button",
-    command = helloWorld
-)
-lbl_result = tk.Label(master=top_frame, text="Placeholder ")
-
-# Set-up the layout using the .grid() geometry manager
-#btn_test.grid(row=0, column=0, padx=10, pady=10)
-#lbl_result.grid(row=0, column=1, padx=10)
+# Left-most UI
+left_frame = tk.Frame(master=window)
+left_frame.grid(row=0, column=0, padx=10, pady=10)
 
 # Create dropdown menu
+topleft_frame = tk.Frame(master=left_frame)
+topleft_frame.grid(row=0, column=0)
 ddtext = tk.StringVar()
 ddtext.set("1")
-sel_squad_lbl = tk.Label(master=top_frame, text="Select Squad: ")
-sel_squad_dd = tk.OptionMenu(top_frame, ddtext, "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+def dd_callback(*args):
+    updateUnitList()
+ddtext.trace("w", dd_callback)
+sel_squad_lbl = tk.Label(master=topleft_frame, text="Select Squad: ")
+sel_squad_dd = tk.OptionMenu(topleft_frame, ddtext, "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+sel_squad_lbl.grid(row=0, column=0, sticky="w")
+sel_squad_dd.grid(row=0, column=1, sticky="e")
 
-# Create the load/ save buttons
-btn_load = tk.Button(
-    master=top_frame,
-    text="Load Squad",
-    command = loadSquad
-)
-
-btn_save = tk.Button(
-    master=top_frame,
-    text="Save Squad",
-    command = saveSquad
-)
-
-btn_clear = tk.Button(
-    master=top_frame,
-    text="Clear Current",
-    command = clearSquad
-)
-
-# Layout the dropdown and label in frm_entry
-sel_squad_lbl.grid(row=1, column=0, sticky="w")
-sel_squad_dd.grid(row=1, column=1, sticky="e")
-btn_load.grid(row=1, column=2, sticky = "e")
-btn_save.grid(row=1, column=3, sticky = "w")
-btn_clear.grid(row=1, column=4, sticky = "w")
-
-## Text that says "Current Squad:"
-sel_squad_text = tk.Label(master=mid_frame, text="Current Squad: ")
-sel_squad_list = tk.Label(master=mid_frame, text="")
-
-# Layout the dropdown and label
-sel_squad_text.grid(row=0, column=0, sticky="w")
-sel_squad_list.grid(row=0, column=1, sticky="e")
-
-# Create the add button, unit name and number entry / labels
+# Add Squad Button
 btn_add = tk.Button(
-    master=bot_frame,
-    text="Add Unit",
+    master=left_frame,
+    text="Add Empty Squad",
+    command = addSquad
+)
+btn_add.grid(row=1, column=0, pady = 10)
+
+# Delete Squad Button
+btn_delete = tk.Button(
+    master=left_frame,
+    text="Delete Current Squad",
+    command = deleteSquad
+)
+btn_delete.grid(row=2, column=0, pady = 10)
+
+# Copy Squad Button
+btn_copy = tk.Button(
+    master=left_frame,
+    text="Copy Current Squad",
+    command = copySquad
+)
+btn_copy.grid(row=3, column=0, pady = 10)
+
+# Paste Squad Button
+btn_paste = tk.Button(
+    master=left_frame,
+    text="Paste Copied Squad",
+    command = pasteSquad
+)
+btn_paste.grid(row=4, column=0, pady = 10)
+
+
+## Second column
+mid_frame = tk.Frame(master=window)
+mid_frame.grid(row=0, column=1, padx=10, pady=10)
+
+curr_lbl = tk.Label(master=mid_frame, text="Units in Selected Squad: ")
+curr_lbl.grid(row=0, column=0, sticky="w")
+curr_squad = tk.Listbox(master=mid_frame)
+curr_squad.grid(row=1, column=0, sticky="w")
+curr_squad.bind('<<ListboxSelect>>', updateSelNum)
+
+
+right_frame = tk.Frame(master=window)
+right_frame.grid(row=0, column=2, padx=10, pady=10)
+
+topright_frame = tk.Frame(master=right_frame)
+topright_frame.grid(row=0, column=0)
+
+num_unit_lbl = tk.Label(master=topright_frame, text="Number of Selected Unit: ")
+num_unit_lbl.grid(row=0, column=0, sticky="w")
+
+numVar = tk.StringVar()
+def num_callback(*args):
+    updateNum()
+numVar.trace("w", num_callback)
+vcmd = (topright_frame.register(checkNumInput), '%S')
+ent_unitnum = tk.Entry(master=topright_frame, width=5, textvariable = numVar, validate='key', vcmd=vcmd)
+ent_unitnum.grid(row=0, column=1, sticky="e")
+
+# Delete Squad Button
+btn_delete = tk.Button(
+    master=right_frame,
+    text="Remove Selected Unit",
+    command = deleteUnit
+)
+btn_delete.grid(row=1, column=0, pady = 10)
+
+
+nameVar = tk.StringVar()
+unitname_lbl = tk.Label(master=right_frame, text="Add Unit with Name: ")
+unitname_lbl.grid(row=2, column=0, pady=(15,0))
+ent_unitname = tk.Entry(master=right_frame, width=25, textvariable = nameVar)
+ent_unitname.grid(row=3, column=0)
+
+    
+
+btn_new = tk.Button(
+    master=right_frame,
+    text="Add Unit to Squad",
     command = addUnit
 )
-unitname_lbl = tk.Label(master=bot_frame, text="Unit Name: ")
-ent_unitname = tk.Entry(master=bot_frame, width=20)
-unitnum_lbl = tk.Label(master=bot_frame, text="Number: ")
-ent_unitnum = tk.Entry(master=bot_frame, width=3)
-
-# Layout
-unitname_lbl.grid(row=0, column=0, sticky="w")
-ent_unitname.grid(row=0, column=1, sticky="e")
-unitnum_lbl.grid(row=0, column=2, sticky="e", padx=10)
-ent_unitnum.grid(row=0, column=3, sticky="w")
-btn_add.grid(row=0, column=4, sticky="e", padx=10)
+btn_new.grid(row=4, column=0, pady=10)
 
 # Create bottom row of features
+foot_frame = tk.Frame(master=right_frame)
+foot_frame.grid(row=5, column=0, pady=(15,0))
+
+playnum_lbl = tk.Label(master=foot_frame, text="Player Number: ")
+playnum_lbl.grid(row=0, column=0, sticky="w")
+
+ddtext2 = tk.StringVar()
+ddtext2.set("0")
+playnum_dd = tk.OptionMenu(foot_frame, ddtext2, "0", "1")
+playnum_dd.grid(row=0, column=1, sticky="e")
+
 btn_gen = tk.Button(
     master=foot_frame,
     text="Generate JSON",
     command = generateJSON
 )
-playnum_lbl = tk.Label(master=foot_frame, text="Player Number: ")
-ent_playnum = tk.Entry(master=foot_frame, width=1)
-
-# Layout
-playnum_lbl.grid(row=0, column=0, sticky="w")
-ent_playnum.grid(row=0, column=1, sticky="e")
 btn_gen.grid(row=0, column=2, sticky="e")
 
 # Run the application
