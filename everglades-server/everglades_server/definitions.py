b5ca1682d6e93badf18cf76f4d8f941856b169b3
Jerold <jrld1999@gmail.com>
2021-04-18 23:12:29 -0400

    Updated

--- everglades-server/everglades_server/definitions.py
+++ everglades-server/everglades_server/definitions.py
@@ -74,9 +74,6 @@ class EvgUnitDefinition:
         self.jamming = kwargs.get('jamming',0)
         self.commander_damage = kwargs.get('commander_damage',0)
         self.commander_speed = kwargs.get('commander_speed',0)
-        self.commander_control = kwargs.get('commander_control',0)
-        self.self_repair = kwargs.get('self_repair',0)
-
         self.recon = kwargs.get('recon', 0)
         self.speedbonus_controlled_ally = kwargs.get('speedbonus_controlled_ally', 0)
         self.speedbonus_controlled_enemy = kwargs.get('speedbonus_controlled_enemy', 0)
